553117f3ea93e69fff48edc1455c48bdbb9060d2
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2021-11-28 00:56:36 -0300

    implemented bist test s1 and s2

--- app/bubble/bubble.c
+++ app/bubble/bubble.c
@@ -1,8 +1,17 @@
+// https://en.wikipedia.org/wiki/Bitwise_operations_in_C
+
 #include <hellfire.h>
 
+#define READ_ONLY 1;
+#define BUBBBLESORT_ADDR_SIZE 18
+
+mutex_t m;
 
 int arr[] = {19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 int arr_size = sizeof(arr)/sizeof(arr[0]);
+int bist_test_counter = 0;
+
+
 
 
 // A function to implement bubble sort
@@ -18,7 +27,6 @@ void bubbleSort(int arr[], int n)
 {
     int i, j, temp;
     for (i = 0; i < n-1; i++)
-
         // Last i elements are already in place
         for (j = 0; j < n-i-1; j++)
             if (arr[j] > arr[j+1]){
@@ -37,11 +45,68 @@ void printArray(int arr[], int size)
     printf("\n");
 }
 
-void bist(void) {
-    printf("I am bist\n");
-    sabotador(arr, arr_size);
-    //sabotador(&bubbleSort, 18); // 18 é a quantidade de comandos assembly que implementam o bubblesort
+int bist_test(int *addr, int addr_size){
+    int i;
+    int value;
+    //S1
+    printf("S1:\t\t    Ra\t\tWa(not)\t\tWa\t\tWa(not)\n");
+    for(i=0;i<addr_size;i++){
+        //Ra
+        value = *addr;
+        printf("i:%2d *adr:%08x: %08x", i, addr, value); 
 
+        //Wa(not)
+        *addr = ~(value); 
+        printf("\t%08x", *addr); 
+
+        //Wa
+        *addr = value; 
+        printf("\t%08x", *addr); 
+
+        //Wa(not)
+        *addr = ~(value); 
+        printf("\t%08x\n", *addr); 
+        addr++; 
+    }
+    addr -= addr_size; // Faz com que o ponteiro volte para o inicio do endereço que está sendo testado
+    
+    //S2
+    printf("\nS2:\t\t    Ra(not)\tWa\t\tRa\t\tWa(not)\n");
+    for(i=0;i<addr_size;i++){
+        //Ra(not)
+        value = ~(*addr);
+        printf("i:%2d *adr:%08x: %08x", i, addr, value); 
+
+        //Wa
+        *addr = value; 
+        printf("\t%08x", *addr); 
+
+        //Ra
+        value = *addr;
+        printf("\t%08x", value); 
+
+        //Wa(not)
+        *addr = ~(value); 
+        printf("\t%08x\n", *addr); 
+        addr++; 
+    }
+    addr -= addr_size; // Faz com que o ponteiro volte para o inicio do endereço que está sendo testado
+    
+}
+//printf("i:%2d *adr:%08x: %08x\n", i, addr, *addr);
+
+void bist(void) {
+    int bist_test_sum, bist_signature_sum;
+
+    printf("\n------------ BIST TEST ------------(%d)\n", bist_test_counter++);
+    bist_test(&bubbleSort, BUBBBLESORT_ADDR_SIZE);
+
+    //sabotador(arr, arr_size);
+
+    //sabotador(&bubbleSort, 18); // 18 é a quantidade de comandos assembly que implementam o bubblesort
+    printf("\n---------------------------------\n");
+    
+    delay_ms(1000);
     for(;;);
     //hf_kill(hf_selfid());
 }
@@ -68,8 +133,6 @@ void sabotador(int *aux, int depth){
 }
 
 void main_routine(void) {
-    // Set random seed
-    srand(42); //srand(time(NULL)); 
 
     printf("\nbubbleSort addr: %08x \n", &bubbleSort);
     printf("arr addr: %08x \n", arr);
@@ -88,6 +151,9 @@ void main_routine(void) {
 }
 
 void app_main(void){
-    hf_spawn(bist, 0, 0, 0, "bist thread", 2048);
-    hf_spawn(main_routine, 0, 0, 0, "main_routine thread", 2048);
+    // Set random seed
+    srand(42); //srand(time(NULL)); 
+
+    hf_spawn(bist, 8, 2, 8, "bist thread", 2048);
+    hf_spawn(main_routine, 4, 1, 4, "main_routine thread", 2048);
 }
